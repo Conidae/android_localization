7b4f342d510f695b546567e20b3f8344a2639aef
Conidae <Conidae52@gmail.com>
2023-11-09 17:32:36 +0900

    create mono_ekf mode

---
+++ launch/only_android_localization.launch.py
@@ -0,0 +1,57 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    ekf_config_path=os.path.join(get_package_share_directory('android_localization'),'config','mono_ekf_setting.yaml')
+
+    return LaunchDescription([
+        Node(
+            package='android_localization',
+            executable='android_vps_node'
+        ),
+        Node(
+            package='android_localization',
+            executable='android_pose_node'
+        ),
+        Node(
+            package='android_localization',
+            executable='android_imu_node'
+        ),
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node_map',
+            output='screen',
+            parameters=[ekf_config_path],
+            remappings=[('odometry/filtered', 'odometry/global'),
+                        ('/set_pose', '/initialpose')]),
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node_odom',
+            output='screen',
+            parameters=[ekf_config_path],
+            remappings=[('odometry/filtered', 'odometry/local'),
+                        ('/set_pose', '/initialpose')]),
+        Node(
+            package='tf2_ros',
+            executable = 'static_transform_publisher',
+            output = 'screen',
+            arguments = ['0.0','0.0','0.0','0.0','0.0','0.0','map','odom']
+        ),                
+        Node(
+            package='robot_localization',
+            executable='navsat_transform_node',
+            name='navsat_transform',
+            output='screen',
+            parameters=[ekf_config_path],
+            remappings=[('imu', 'android/imu'),
+                        ('gps/fix', 'gps/fix'), 
+                        ('gps/filtered', 'gps/filtered'),
+                        ('oadometry/gps', 'odometry/gps'),
+                        ('odometry/filtered', 'odometry/global')])
+                        
+    ])
